3cc51e097c17f39117c2c6725cfa13632bc27594
vijay <vijaybalajitheboss@gmail.com>
2020-06-05 20:23:52 +0530

    Structured the program, and added new window

--- src/Typing-Speed-Test.py
+++ src/Typing-Speed-Test.py
@@ -33,7 +33,20 @@ class application():
     def launch(self):
         self.new_window = Toplevel(self.root) 
         self.root.withdraw()
-        # self.app = SecondPage(self.new_window self.choiceVar.get())
+        self.app = SecondPage(self.new_window, self.choiceVar.get())
+
+class SecondPage:
+    def __init__(self,window,choice):
+        self.window = window
+        self.window.state("zoomed")
+        self.window.grid_rowconfigure(0,weight=0)
+        self.window.grid_columnconfigure(0,weight=1)
+        self.choice = choice
+
+        window.protocol("WM_DELETE_WINDOW",self.closing)
+
+    def closing(self):
+        sys.exit(0)
 
 if __name__ == "__main__":
     window = Tk()
