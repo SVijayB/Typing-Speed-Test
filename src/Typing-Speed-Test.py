9c4e959ae127f205f7fad985c3903ff6ff9dbd5f
vijay <vijaybalajitheboss@gmail.com>
2020-06-06 08:45:56 +0530

    Added selection page as a seperate file

--- src/Typing-Speed-Test.py
+++ src/Typing-Speed-Test.py
@@ -1,39 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import time
-
-class application():
-    def __init__(self,root):
-        self.root = root
-        self.root.grid_rowconfigure(0,weight=2)
-        self.root.grid_columnconfigure(0,weight=1)
-        self.root.configure(bg="black")
-
-        self.heading = Label(self.root,text = "TYPING SPEED TEST",fg = "green",
-        bg = "black", font=("Castellar", 70))
-        self.heading.grid(pady = (0,10))
-
-        self.choice = Label(self.root,text="SELECT TYPING MODE",
-        bg = "black", fg = "white", font=("Algerian",40))
-        self.choice.grid(pady = (0,100))
-
-        Choices = [(" WARMUP ",1),(" EASY ",2),(" HARD ",2)]
-        self.choiceVar = StringVar()
-        self.choiceVar.set(1)
-
-        for text,mode in Choices:
-            self.type = Radiobutton(self.root, text=text, font=("Northwest old", 15), 
-            fg = "#FF6347",bg = "black",variable=self.choiceVar, value = mode)
-            self.type.grid(pady = (0,30))
-        
-        self.Start = Button(self.root, text="Start", width = 10, command = self.launch, 
-        fg = "white", bg = "black", font=("Bell MT", 15))
-        self.Start.grid(pady=(30,70))
-
-    def launch(self):
-        self.new_window = Toplevel(self.root) 
-        self.root.withdraw()
-        self.app = SecondPage(self.new_window, self.choiceVar.get())
+from Modules.SelectionPage import SelectionPage
 
 class SecondPage:
     def __init__(self,window,choice):
@@ -51,5 +19,5 @@ class SecondPage:
 if __name__ == "__main__":
     window = Tk()
     window.state("zoomed")
-    app = application(window)
+    app = SelectionPage(window)
     window.mainloop()
