26f895aef24763cebc7f25bc41da09212b91f1d6
vijay <vijaybalajitheboss@gmail.com>
2020-06-09 11:20:43 +0530

    Printing only specific basic words

--- src/Modules/Words.py
+++ src/Modules/Words.py
@@ -7,7 +7,11 @@ def words():
     response = urllib.request.urlopen(url)
     data = str(response.read().decode())
     words = data.splitlines()
-    randwords = random.sample(words,200)
+    basic_words = []
+    for x in words:
+        if(len(x) < 7 and len(x)>3):
+            basic_words.append(x)
+    randwords = random.sample(basic_words,200)
     result = ""
     for x in randwords:
         result = x + "\n" +result
