80c47e9d346766a8981d4c4009fc867a39632773
vijay <vijaybalajitheboss@gmail.com>
2020-06-09 14:30:02 +0530

    Added all the different modes to get random words

--- src/Modules/Words.py
+++ src/Modules/Words.py
@@ -2,14 +2,21 @@ import urllib.request
 import random
 import time
 
-def words():
+def words(choice):
     url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
     response = urllib.request.urlopen(url)
     data = str(response.read().decode())
     words = data.splitlines()
     basic_words = []
+    j = 3
+    if(choice == 1):
+        i = 20
+    elif(choice == 2):
+        i = 7
+    elif(choice == 3):
+        i = 12; j = 5
     for x in words:
-        if(len(x) < 7 and len(x)>3):
+        if(len(x) < i and len(x) > j):
             basic_words.append(x)
     randwords = random.sample(basic_words,200)
     result = ""
@@ -17,4 +24,4 @@ def words():
         result = x + "\n" +result
     return result
 
-print(words())
+print(words(2))
