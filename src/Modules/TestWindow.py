c0dfb7918ffbe49db7f3217c64fd6f4e6a25bcf2
vijay <vijaybalajitheboss@gmail.com>
2020-06-18 07:27:06 +0530

    Updated layout and positions of widgets

--- src/Modules/TestWindow.py
+++ src/Modules/TestWindow.py
@@ -6,23 +6,24 @@ window.title("Test Window")
 window.config(bg = "black")
 window.geometry("600x400")
 
-colour = ["Red","Blue","Green","Pink","Yellow","Orange","White","Purple","Brown"]
+colour = ["Red","Green","Pink","Yellow","Orange","White","Purple","Brown"]
 score = 0
 
 current_word = StringVar()
-entry_field = Entry(window, textvariable=current_word, font = "Algerian",
+entry_field = Entry(window, textvariable=current_word, width= 40, font = "Algerian",
     bg = "black", insertbackground = "red", fg='white')
+entry_field.focus()
 
-board = Label(window,text="Press enter to start",width=600,height=400,
-        font="Algerian 25",fg = "green",bg = "black")
+board = Label(window,text="Press enter \nto start",width=600,height=400,
+        font="Algerian 40",fg = "green",bg = "black")
 board.pack()
 
-question = Label(window,font = "Algerian",bg = "black")
-question.place(x=230,y=50)
+question = Label(window,font = "Algerian 40",bg = "black")
+question.place(x=230,y=100)
 
 time_up_message = Label(window)
 
-score_label = Label(window,text = "Score : 0", font = "Agency",fg = "green", bg = "black")
+score_label = Label(window,text = "Score : 0", font = "Agency 25",fg = "green", bg = "black")
 
 time = 61
 
@@ -33,7 +34,7 @@ def final_score(score):
 	
 def result():
     speed = Label(window,text = "Speed : " + str(round(60/(score))) + "wpm", 
-        width = 200, height = 100, fg = "green", font = "Algerian",bg = "back")
+        width = 200, height = 100, fg = "green", font = "Algerian 50",bg = "black")
     speed.pack()
     time_up_message.forget()
     score_label.forget()
@@ -42,17 +43,17 @@ def time_decrement():
     global time
     if time>0:
         time = time - 1
-        time_label.config(text = "Time Left : " + str(time),font = "Agency", 
+        time_label.config(text = "Time Left : " + str(time),font = "Agency 25", 
             bg = "black", fg = "orange")
         time_label.place(x = 2, y = 2)
         window.after(1000,time_decrement)
     else:
         entry_field.place_forget()
-        time_up_message.config(text = "Time's Up", font = "Algerian", bg = "black", 
-            fg = "red", width = 600, height = 600)
-        time_up_message.pack()
         score_label.place_forget()
         time_label.place_forget()
+        time_up_message.config(text = "Time's Up", font = "Algerian 50", bg = "black", 
+            fg = "red", width = 600, height = 600)
+        time_up_message.pack()
         window.after(2000,result)
 
 class test():	
@@ -64,8 +65,8 @@ class test():
                 time_decrement()
             word = random.randint(0,9)
             question.config(text = str("Word"),fg = colour[word])
-            entry_field.place(x=120,y=200)
-            score_label.place(x=400,y=2)
+            entry_field.place(x=120,y=250)
+            score_label.place(x=440,y=2)
         
             def correct(event):
                 global score
